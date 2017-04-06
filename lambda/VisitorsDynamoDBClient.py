3acf4975a6200537c13d64cce781d657e273f6ff
Kay Hau <kyhau@users.noreply.github.com>
2017-04-06 18:21:47 +1000

    Update and rename VisitorManager.py to VisitorsDynamoDBClient.py

--- lambda/VisitorsDynamoDBClient.py
+++ lambda/VisitorsDynamoDBClient.py
@@ -1,24 +1,34 @@
 from __future__ import print_function
-
 import boto3
 import json
+import uuid
+from datetime import datetime
 
 print('Loading function')
 
 
 def handler(event, context):
-    '''Provide an event that contains the following keys:
+    """Provide an event that contains the following keys:
 
       - operation: one of the operations in the operations dict below
       - tableName: required for operations that interact with DynamoDB
       - payload: a parameter to pass to the operation being performed
-    '''
+    """
     print("Received event: " + json.dumps(event, indent=2))
 
     operation = event['operation']
 
     if 'tableName' in event:
         dynamo = boto3.resource('dynamodb').Table(event['tableName'])
+
+    if operation == 'create':
+        event['payload']['Item']['UUID'] = str(uuid.uuid4())
+
+    if 'timestampField' in event:
+        if operation == 'create':
+            event['payload']['Item'][event['timestampField']] = datetime.now().strftime("%Y-%m-%d,%H:%M")
+        elif operation == 'update':
+            event['payload'].update({'AttributeUpdates': { event['timestampField']: {'Value': datetime.now().strftime("%Y-%m-%d,%H:%M")}}})
 
     operations = {
         'create': lambda x: dynamo.put_item(**x),
