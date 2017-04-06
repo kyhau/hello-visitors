b6df20fb48fb3416ae92870497691d5214048b19
Kay Hau <kyhau@users.noreply.github.com>
2017-04-06 18:20:44 +1000

    Update hello-visitors.js

--- site/hello-visitors.js
+++ site/hello-visitors.js
@@ -1,9 +1,66 @@
+function setButtonsVisibility(showReset) {
+  if (showReset == true) {
+    document.querySelector('.resetbtn').style.visibility='visible';
+    document.querySelector('.signinModelBtn').style.visibility='hidden';
+    document.querySelector('.signoutModelBtn').style.visibility='hidden';
+  }
+  else {
+    document.querySelector('.resetbtn').style.visibility='hidden';
+    document.querySelector('.signinModelBtn').style.visibility='visible';
+    document.querySelector('.signoutModelBtn').style.visibility='visible';
+  }
+}
+
+function clearInputs() {
+  document.getElementById("companyname").value = "";
+  document.getElementById("fullname").value = "";
+}
+
+
+var signinModelBtn = document.querySelector('.signinModelBtn');
+signinModelBtn.onclick = function(e) {
+  document.getElementById('signinModel').style.display='block';
+}
+
+var signoutModelBtn = document.querySelector('.signoutModelBtn');
+signoutModelBtn.onclick = function(e) {
+  document.getElementById('signoutModel').style.display='block';
+
+  var selectOptions = document.getElementById('visitors');
+
+  // Reset existing options, if any.
+  selectOptions.options.length = 0;
+  var defaultOption = new Option("Choose here", "NoName", true, true);
+  defaultOption.setAttribute('disabled', 'disabled');
+  selectOptions.appendChild(defaultOption);
+
+  // Retrieve data from API Gateway
+  var apigClient = apigClientFactory.newClient();
+  var params = {};
+  var body = {};
+  var additionalParams = {};
+
+  apigClient.visitorsGet(params, body, additionalParams)
+    .then(function(result){
+      console.log(JSON.stringify(result));
+
+      for(i=0; i<result['data']['Count']; ++i) {
+        items = result['data']['Items'];
+        opt = new Option(items[i]['FullName'], items[i]['UUID']);
+        selectOptions.appendChild(opt);
+      }
+    }).catch(function(result){
+      console.error(JSON.stringify(result));
+      document.querySelector('.msg').innerHTML = "Failed to retrieve visitors. Please try again.";
+    });
+}
+
 // Get the modal
 var signin_model = document.getElementById('signinModel');
 window.onclick = function(event) {
   if (event.target == signin_model) {
     signin_model.style.display = "none";
-    document.getElementById("feedback").value = document.getElementById("fullname").value;
+    document.getElementById("msg").value = document.getElementById("fullname").value;
   }
 }
 
@@ -34,39 +91,37 @@ cancelSignoutBtn.onclick = function(e) {
   document.getElementById('signoutModel').style.display='none';
 }
 
-
 var signinBtn = document.querySelector('.signinbtn');
 signinBtn.onclick = function(e) {
-  var fullnameInput = document.querySelector('#fullname');
-  var companynameInput = document.querySelector('#companyname');
+  var fullname = document.querySelector('#fullname').value;
+  var companyname = document.querySelector('#companyname').value;
 
-  // TODO call api gateway
-  var apigClient = apigClientFactory.newClient();
+  if (companyname == "") {
+    document.querySelector('.msg').innerHTML = "\"Please specify your Company Name. Thank you!\"";
+  }
+  else if (fullname == "") {
+    document.querySelector('.msg').innerHTML = "\"Please specify your Full Name. Thank you!\"";
+  }
+  else {
+    var apigClient = apigClientFactory.newClient();
+    var params = {};
+    var body = {
+      "companyname": companyname,
+      "fullname": fullname
+    };
+    var additionalParams = {};
 
-  var params = {
-    //This is where any header, path, or querystring request params go. The key is the parameter named as defined in the API
-  };
-  var body = {
-    "fullname": fullnameInput.value,
-    "companyname": companynameInput.value,
-  };
-  var additionalParams = {
-    //If there are any unmodeled query parameters or headers that need to be sent with the request you can add them here
-    headers: {},
-    queryParams: {}
-  };
+    apigClient.visitorsPost(params, body, additionalParams)
+      .then(function(result){
+        document.querySelector('.msg').innerHTML = "Welcome " + fullname + "!";
+        clearInputs();
 
-  apigClient.visitorsPost(params, body, additionalParams)
-    .then(function(result){
-      //This is where you would put a success callback
-      document.querySelector('.feedback').innerHTML = "Welcome " + fullnameInput.value + "!";
-
-    }).catch( function(result){
-      //This is where you would put an error callback
-      document.querySelector('.feedback').innerHTML = "Failed to add new visitor. Please try again.";
-    });
-
-  document.querySelector('.resetbtn').style.display='block';
+      }).catch( function(result){
+        console.error(JSON.stringify(result));
+        document.querySelector('.msg').innerHTML = "Failed to add new visitor. Please try again.";
+      });
+  }
+  setButtonsVisibility(true);
   document.getElementById('signinModel').style.display='none';
   // prevent page jumps
   //e.preventDefault();
@@ -74,21 +129,38 @@ signinBtn.onclick = function(e) {
 
 var signoutBtn = document.querySelector('.signoutbtn');
 signoutBtn.onclick = function(e) {
-  var fullnameInput = document.querySelector('#searchname');
+  var selectOptions = document.getElementById('visitors');
 
-  // TODO call api gateway
+  if (selectOptions.value != 'NoName') {
 
-  document.querySelector('.feedback').innerHTML = "Goodbye " + fullnameInput.value + "! Have a nice day!";
-  document.querySelector('.resetbtn').style.display='block';
-  document.getElementById('signoutModel').style.display='none';
+    var uuid = selectOptions[selectOptions.selectedIndex].value
+    var fullName = selectOptions[selectOptions.selectedIndex].text
 
+    var apigClient = apigClientFactory.newClient();
+    var params = {};
+    var body = {
+      "uuid": uuid
+    };
+    var additionalParams = {};
+
+    apigClient.visitorsPut(params, body, additionalParams)
+      .then(function(result){
+        document.querySelector('.msg').innerHTML = "Goodbye " + fullName + "!";
+      }).catch( function(result){
+        console.error(JSON.stringify(result));
+        document.querySelector('.msg').innerHTML = "Failed to signout " + fullName + ". Please try again.";
+      });
+
+    setButtonsVisibility(true);
+    document.getElementById('signoutModel').style.display='none';
+  }
   // prevent page jumps
   //e.preventDefault();
 }
 
 var resetBtn = document.querySelector('.resetbtn');
-resetBtn.style.display='none';
+setButtonsVisibility(false);
 resetBtn.onclick = function(e) {
-  this.style.display='none';
-  feedbackMsg = document.querySelector('.feedback').innerHTML = "";
+  setButtonsVisibility(false);
+  msgMsg = document.querySelector('.msg').innerHTML = "\"Hello visitor!\"";
 }
